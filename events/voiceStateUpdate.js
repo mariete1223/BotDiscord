455b079647c0d1a82a3efbdd4668d37bcfd16626
mariete1223 <mariete1223@outlook.es>
2022-05-06 00:08:43 +0200

    fixing file voicestate

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,4 +1,4 @@
-/*
+
 const { Permissions } = require("discord.js");
 const {getVoiceConnection} = require("@discordjs/voice");
 //modulos para dividir el programa, se devuelve cuando se hace require de otro programa
@@ -47,47 +47,5 @@ module.exports = async (client, oldState, newState) => {
         if((connection || connection1)&& connection.joinCongfig.channelId == o.channelId) connection.destroy();
         return;
 
-    }
-}
-*/
-const { Permissions } = require("discord.js");
-const { getVoiceConnection } = require("@discordjs/voice")
-module.exports = async (client, oldState, newState) => {
-  
-    // Stage Channel self suspress
-    if(newState.id == client.user.id && newState.channelId && newState.channel.type == "GUILD_STAGE_VOICE" && newState.suppress) {
-        if(newState.channel?.permissionsFor(newState.guild.me)?.has(Permissions.FLAGS.MUTE_MEMBERS)) {
-            await newState.guild.me.voice.setSuppressed(false).catch(() => null);
-        } 
-    }
-    
-    if(newState.id == client.user.id) return;
-
-    // Destroy connection if channel gets emtpy
-    function stateChange(one, two) {
-        if(one === false && two === true || one === true && two === false) return true;
-        else return false;
-    }
-    const o = oldState, n = newState;
-    if(stateChange(o.streaming, n.streaming) ||
-      stateChange(o.serverDeaf, n.serverDeaf) ||
-      stateChange(o.serverMute, n.serverMute) ||
-      stateChange(o.selfDeaf, n.selfDeaf) ||
-      stateChange(o.selfMute, n.selfMute) ||
-      stateChange(o.selfVideo, n.selfVideo) ||
-      stateChange(o.suppress, n.suppress)) return;
-    // channel joins
-    if(!o.channelId && n.channelId) {
-        return;
-    }
-    // channel leaves
-    if(!n.channelId && o.channelId || n.channelId && o.channelId) {
-        //obtenemos las conexiones activas || no funciona no recupera el voice channel
-        const connection = getVoiceConnection(n.guild.id);
-        //vemos si hay alguien mas en el canal
-        if(o.channel.members.filter(m => !m.user.bot && !m.voice.selfDeaf && !m.voice.serverDeaf).size >= 1) return;
-        //si no lo hay cerramos la conexion
-        if(connection && connection.joinConfig.channelId == o.channelId) connection.destroy();
-        return;
     }
 }
